d775479948b825846bcfcfabe54cf564875f61a1
Mark Spanbroek <mark@spanbroek.net>
2017-05-25 23:14:20 +0200

    Remove unnecessary REST server

--- .travis-runner.sh
+++ .travis-runner.sh
@@ -5,7 +5,6 @@ startdir="$(cd `dirname $0`; pwd)"
 ( cd ${startdir}/src/app-contracts && yarn install )
 ( cd ${startdir}/src/app-contracts && yarn run testrpc & )
 ( cd ${startdir}/src/app-webapp && yarn install )
-( cd ${startdir}/src/app-restserver && yarn install )
 
 yarn install
 

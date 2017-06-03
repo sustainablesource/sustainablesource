9b6afe7800b679f299235e81e91f030e75acae9a
Mark Spanbroek <mark@spanbroek.net>
2017-06-03 23:14:20 +0200

    Give out license when license fee has been paid

--- src/app-contracts/contracts/SustainableSource.sol
+++ src/app-contracts/contracts/SustainableSource.sol
@@ -2,4 +2,9 @@ pragma solidity ^0.4.8;
 
 contract SustainableSource {
 
+  mapping (address => bool) public hasLicense;
+
+  function payLicenseFeeFor(address account) payable {
+      hasLicense[account] = true;
+  }
 }
--- src/app-contracts/test/sustainable-source.js
+++ src/app-contracts/test/sustainable-source.js
@@ -1,9 +1,28 @@
 /* eslint-env mocha */
+/* global web3 */
 const expect = require('chai').expect
 const SustainableSource = artifacts.require('SustainableSource.sol')
 
-contract('SustainableSource', function () {
+contract('SustainableSource', function (accounts) {
+  const person = accounts[1]
+  const fee = web3.toWei(1, 'ether')
+
+  let sustainable
+
+  beforeEach(async function () {
+    sustainable = await SustainableSource.new()
+  })
+
   it('is deployed', async function () {
     expect(await SustainableSource.deployed()).to.exist
+  })
+
+  it('does not give out a license initially', async function () {
+    expect(await sustainable.hasLicense(person)).to.be.false
+  })
+
+  it('gives out a license when license fee was paid', async function () {
+    await sustainable.payLicenseFeeFor(person, {value: fee})
+    expect(await sustainable.hasLicense(person)).to.be.true
   })
 })

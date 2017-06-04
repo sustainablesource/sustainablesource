7fd6a80218b0f5f890cfd4abe5a2d21ca6697509
Mark Spanbroek <mark@spanbroek.net>
2017-06-04 08:07:13 +0200

    Check that amount of ether paid matches the license fee

--- src/app-contracts/contracts/SustainableSource.sol
+++ src/app-contracts/contracts/SustainableSource.sol
@@ -3,8 +3,16 @@ pragma solidity ^0.4.8;
 contract SustainableSource {
 
   mapping (address => bool) public hasLicense;
+  uint licenseFee;
+
+  function SustainableSource(uint licenseFeeInWei) {
+    licenseFee = licenseFeeInWei;
+  }
 
   function payLicenseFeeFor(address account) payable {
+      if (msg.value != licenseFee) {
+        throw;
+      }
       hasLicense[account] = true;
   }
 }
--- src/app-contracts/test/sustainable-source.js
+++ src/app-contracts/test/sustainable-source.js
@@ -1,16 +1,20 @@
 /* eslint-env mocha */
 /* global web3 */
-const expect = require('chai').expect
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+const expect = chai.expect
 const SustainableSource = artifacts.require('SustainableSource.sol')
+
+chai.use(chaiAsPromised)
 
 contract('SustainableSource', function (accounts) {
   const person = accounts[1]
-  const fee = web3.toWei(1, 'ether')
+  const fee = web3.toWei(10, 'finney')
 
   let sustainable
 
   beforeEach(async function () {
-    sustainable = await SustainableSource.new()
+    sustainable = await SustainableSource.new(fee)
   })
 
   it('is deployed', async function () {
@@ -24,5 +28,17 @@ contract('SustainableSource', function (accounts) {
   it('gives out a license when license fee was paid', async function () {
     await sustainable.payLicenseFeeFor(person, {value: fee})
     expect(await sustainable.hasLicense(person)).to.be.true
+  })
+
+  it('throws when less than license fee was paid', async function () {
+    const tooLittle = web3.toWei(9, 'finney')
+    const call = sustainable.payLicenseFeeFor(person, {value: tooLittle})
+    await expect(call).to.eventually.be.rejected
+  })
+
+  it('throws when more than license fee was paid', async function () {
+    const tooMuch = web3.toWei(11, 'finney')
+    const call = sustainable.payLicenseFeeFor(person, {value: tooMuch})
+    await expect(call).to.eventually.be.rejected
   })
 })

cd509f2e63638ef0e8c2014ced12a0b081ea0d16
Mark Spanbroek <mark@spanbroek.net>
2017-08-07 21:00:29 +0200

    Refactor: extract functionality for stubbing and spying on Oraclize calls into separate contract

---
+++ src/app-contracts/contracts/OraclizeSpy.sol
@@ -0,0 +1,53 @@
+pragma solidity^0.4.6;
+
+contract OraclizeSpy {
+    string public latestOraclizeDataSource;
+    string public latestOraclizeArg;
+    uint public latestOraclizeGasLimit;
+    byte public latestProofType;
+
+    bytes32 queryIdToReturn;
+    address oraclizeAddressToReturn;
+    mapping (string => mapping (uint => uint)) oraclizePriceToReturn;
+
+    function alwaysReturnOraclizeQueryId(bytes32 queryId) {
+        queryIdToReturn = queryId;
+    }
+
+    function alwaysReturnOraclizeAddress(address oraclizeAddress) {
+        oraclizeAddressToReturn = oraclizeAddress;
+    }
+
+    function alwaysReturnOraclizePrice(
+        string datasource,
+        uint gaslimit,
+        uint price)
+    {
+      oraclizePriceToReturn[datasource][gaslimit] = price;
+    }
+
+    function oraclize_query(string datasource, string arg, uint gaslimit)
+        internal
+        returns (bytes32)
+    {
+        latestOraclizeDataSource = datasource;
+        latestOraclizeArg = arg;
+        latestOraclizeGasLimit = gaslimit;
+        return queryIdToReturn;
+    }
+
+    function oraclize_setProof(byte proofType) internal {
+        latestProofType = proofType;
+    }
+
+    function oraclize_cbAddress() internal returns (address) {
+        return oraclizeAddressToReturn;
+    }
+
+    function oraclize_getPrice(string datasource, uint gaslimit)
+        internal
+        returns (uint)
+    {
+        return oraclizePriceToReturn[datasource][gaslimit];
+    }
+}

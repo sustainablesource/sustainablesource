3885c2e64f27fb82d8fc243682f79e06b9e67d1f
Mark Spanbroek <mark@spanbroek.net>
2017-09-09 20:56:05 +0200

    Refactor: move user attestation tests closer together

--- src/app-contracts/test/github-integration.js
+++ src/app-contracts/test/github-integration.js
@@ -5,29 +5,12 @@ const isTestNetwork = require('./test-networks').isTestNetwork
 const Users = artifacts.require('Users.sol')
 
 contract('GitHub', function (accounts) {
-  const gistId = 'a85344feaf0141e1b393f399d8c01318'
   const address = '0x7792eba89dd0facd048329955c5855d52554b788'
-  const username = 'markspanbroek'
-
-  let users
-  let price
-
-  beforeEach(async function () {
-    users = await Users.deployed()
-    price = await users.attestationPrice()
-  })
 
   describe('integration testing environment', function () {
     it('has the correct main account', async function () {
       if (await isTestNetwork(web3)) {
         expect(accounts[0]).to.equal(address)
-      }
-    })
-
-    it('has sufficient funds', async function () {
-      if (await isTestNetwork(web3)) {
-        const balance = await web3.eth.getBalance(address)
-        expect(balance.toNumber()).to.be.above(price.toNumber())
       }
     })
   })
@@ -37,22 +20,42 @@ contract('GitHub', function (accounts) {
       this.timeout(5 * 60 * 1000)
     })
 
-    it('attests with a correct gist', async function () {
-      async function poll () {
-        if (await users.users(username) !== address) {
-          await wait(5000)
+    describe('user attestation', function () {
+      const gistId = 'a85344feaf0141e1b393f399d8c01318'
+      const username = 'markspanbroek'
+
+      let users
+      let price
+
+      beforeEach(async function () {
+        users = await Users.deployed()
+        price = await users.attestationPrice()
+      })
+
+      it('has sufficient funds', async function () {
+        if (await isTestNetwork(web3)) {
+          const balance = await web3.eth.getBalance(address)
+          expect(balance.toNumber()).to.be.above(price.toNumber())
+        }
+      })
+
+      it('attests with a correct gist', async function () {
+        async function poll () {
+          if (await users.users(username) !== address) {
+            await wait(5000)
+            await poll()
+          }
+        }
+        async function wait (milliseconds) {
+          return new Promise(function (resolve) {
+            setTimeout(resolve, milliseconds)
+          })
+        }
+        if (await isTestNetwork(web3)) {
+          await users.attest(username, gistId, { value: price })
           await poll()
         }
-      }
-      async function wait (milliseconds) {
-        return new Promise(function (resolve) {
-          setTimeout(resolve, milliseconds)
-        })
-      }
-      if (await isTestNetwork(web3)) {
-        await users.attest(username, gistId, { value: price })
-        await poll()
-      }
+      })
     })
   })
 })

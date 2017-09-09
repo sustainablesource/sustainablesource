ceb7f842cb47a7415a25861dafaf356a9a9511a1
Mark Spanbroek <mark@spanbroek.net>
2017-09-09 21:15:16 +0200

    Add integration test for pull request registration

--- src/app-contracts/test/github-integration.js
+++ src/app-contracts/test/github-integration.js
@@ -3,9 +3,11 @@
 const expect = require('chai').expect
 const isTestNetwork = require('./test-networks').isTestNetwork
 const Users = artifacts.require('Users.sol')
+const PullRequests = artifacts.require('PullRequests.sol')
 
 contract('GitHub', function (accounts) {
   const address = '0x7792eba89dd0facd048329955c5855d52554b788'
+  const username = 'markspanbroek'
 
   describe('integration testing environment', function () {
     it('has the correct main account', async function () {
@@ -22,7 +24,6 @@ contract('GitHub', function (accounts) {
 
     describe('user attestation', function () {
       const gistId = 'a85344feaf0141e1b393f399d8c01318'
-      const username = 'markspanbroek'
 
       let users
       let price
@@ -46,16 +47,51 @@ contract('GitHub', function (accounts) {
             await poll()
           }
         }
-        async function wait (milliseconds) {
-          return new Promise(function (resolve) {
-            setTimeout(resolve, milliseconds)
-          })
-        }
         if (await isTestNetwork(web3)) {
           await users.attest(username, gistId, { value: price })
           await poll()
         }
       })
     })
+
+    describe('pull request registration', function () {
+      const pullRequestId = 3
+
+      let pullRequests
+      let price
+
+      beforeEach(async function () {
+        pullRequests = await PullRequests.deployed()
+        price = await pullRequests.registrationPrice()
+      })
+
+      it('has sufficient funds', async function () {
+        if (await isTestNetwork(web3)) {
+          const balance = await web3.eth.getBalance(address)
+          expect(balance.toNumber()).to.be.above(price.toNumber())
+        }
+      })
+
+      it('registers a pull request', async function () {
+        async function poll () {
+          const creator = await pullRequests.creators(pullRequestId)
+          const isMerged = await pullRequests.isMerged(pullRequestId)
+          if (creator !== username || !isMerged) {
+            await wait(500)
+            await poll()
+          }
+        }
+        if (await isTestNetwork(web3)) {
+          await pullRequests.register(pullRequestId, username, { value: price })
+          await poll()
+        }
+      })
+    })
   })
+
+  async function wait (milliseconds) {
+    return new Promise(function (resolve) {
+      setTimeout(resolve, milliseconds)
+    })
+  }
 })

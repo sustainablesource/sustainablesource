efb81f77835f89a0287230b92380280dc7b76183
Mark Spanbroek <mark@spanbroek.net>
2018-02-23 13:12:27 +0100

    Register shares and shareholders

--- src/app-contracts/contracts/Payout.sol
+++ src/app-contracts/contracts/Payout.sol
@@ -1,5 +1,40 @@
 pragma solidity ^0.4.8;
 
-contract Payout {
+import "./UsersInterface.sol";
+import "./PullRequestsInterface.sol";
 
+contract Payout {
+    uint public totalNumberOfShares;
+    address[] shareholders;
+    mapping (address => uint) shares;
+
+    UsersInterface users;
+    PullRequestsInterface pullRequests;
+
+    function Payout(UsersInterface users_, PullRequestsInterface pullRequests_) public {
+        users = users_;
+        pullRequests = pullRequests_;
+    }
+
+    function registerShare(uint pullRequestId) public {
+        totalNumberOfShares++;
+        bytes32 creatorHash = pullRequests.creatorHash(pullRequestId);
+        address shareholder = users.userByHash(creatorHash);
+        if (shares[shareholder] == 0) {
+            shareholders.push(shareholder);
+        }
+        shares[shareholder]++;
+    }
+
+    function numberOfShareholders() public constant returns (uint) {
+        return shareholders.length;
+    }
+
+    function getShareHolder(uint index) public constant returns (address) {
+        return shareholders[index];
+    }
+
+    function numberOfShares(address shareholder) public constant returns (uint) {
+        return shares[shareholder];
+    }
 }
--- src/app-contracts/test/payout.js
+++ src/app-contracts/test/payout.js
@@ -1,9 +1,76 @@
 /* eslint-env mocha */
 const Payout = artifacts.require('Payout.sol')
+const Users = artifacts.require('UsersFake.sol')
+const PullRequests = artifacts.require('PullRequestsFake.sol')
 const expect = require('chai').expect
 
-contract('Payout', function () {
+contract('Payout', function (accounts) {
+  let payout
+  let users
+  let pullRequests
+
+  beforeEach(async function () {
+    pullRequests = await PullRequests.new()
+    users = await Users.new()
+    payout = await Payout.new(users.address, pullRequests.address)
+  })
+
   it('is deployed', async function () {
     expect(await Payout.deployed()).to.exist
+  })
+
+  it('has 0 shares by default', async function () {
+    const totalNumberOfShares = await payout.totalNumberOfShares()
+    const numberOfShareholders = await payout.numberOfShareholders()
+    expect(totalNumberOfShares.toNumber()).to.equal(0)
+    expect(numberOfShareholders.toNumber()).to.equal(0)
+  })
+
+  context('when registering a merged pull request', function () {
+    const pullRequestId = 42
+    const creator = 'a creator'
+    const creatorAddress = accounts[1]
+
+    beforeEach(async function () {
+      await pullRequests.setCreator(pullRequestId, creator)
+      await pullRequests.setIsMerged(pullRequestId, true)
+      await users.setUser(creator, creatorAddress)
+      await payout.registerShare(pullRequestId)
+    })
+
+    it('increments the number of shares', async function () {
+      const totalNumberOfShares = await payout.totalNumberOfShares()
+      expect(totalNumberOfShares.toNumber()).to.equal(1)
+    })
+
+    it('registers the share holder', async function () {
+      const numberOfShareholders = await payout.numberOfShareholders()
+      const shareHolder = await payout.getShareHolder(0)
+      expect(numberOfShareholders.toNumber()).to.equal(1)
+      expect(shareHolder).to.equal(creatorAddress)
+    })
+
+    it('registers the share', async function () {
+      const numberOfShares = await payout.numberOfShares(creatorAddress)
+      expect(numberOfShares.toNumber()).to.equal(1)
+    })
+
+    context('when registering another pull request', function () {
+      beforeEach(async function () {
+        await pullRequests.setCreator(43, creator)
+        await pullRequests.setIsMerged(43, true)
+        await payout.registerShare(43)
+      })
+
+      it('increases the number of shares', async function () {
+        const numberOfShares = await payout.numberOfShares(creatorAddress)
+        expect(numberOfShares.toNumber()).to.equal(2)
+      })
+
+      it('does not re-register the shareholder', async function () {
+        const numberOfShareholders = await payout.numberOfShareholders()
+        expect(numberOfShareholders.toNumber()).to.equal(1)
+      })
+    })
   })
 })

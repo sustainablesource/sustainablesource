01ec8f9680f86585a96271b89e29030614e37e8a
Mark Spanbroek <mark@spanbroek.net>
2018-02-25 12:20:37 +0100

    Deploy Contributions contract with correct dependencies

--- src/app-contracts/migrations/2_deploy_contracts.js
+++ src/app-contracts/migrations/2_deploy_contracts.js
@@ -3,9 +3,14 @@ const Users = artifacts.require('Users.sol')
 const PullRequests = artifacts.require('PullRequests.sol')
 const Contributions = artifacts.require('Contributions.sol')
 
+async function deploy (deployer) {
+  await deployer.deploy(Licenses)
+  await deployer.deploy(Users)
+  await deployer.deploy(PullRequests, 'sustainablesource/sustainablesource')
+  await deployer.deploy(Contributions, Users.address, PullRequests.address)
+}
+
 module.exports = function (deployer) {
-  deployer.deploy(Licenses)
-  deployer.deploy(Users)
-  deployer.deploy(PullRequests, 'sustainablesource/sustainablesource')
-  deployer.deploy(Contributions)
+  // workaround for https://github.com/trufflesuite/truffle/issues/501:
+  deployer.then(() => deploy(deployer))
 }

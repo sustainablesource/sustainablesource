a50b3f7c6c0b89c8ade2854ea68c1c1969b0d693
Mark Spanbroek <mark@spanbroek.net>
2018-09-09 14:54:44 +0200

    contracts: export compiled Users contract

--- contracts/index.js
+++ contracts/index.js
@@ -1,26 +1,2 @@
-const contract = require('truffle-contract')
-
-const contractNames = []
-
-let contracts = {}
-contractNames.forEach(function (name) {
-  const json = require(`./build/contracts/${name}.json`)
-  contracts[name] = contract(json)
-})
-
-function setProvider (web3Provider) {
-  for (let name in contracts) {
-    contracts[name].setProvider(web3Provider)
-  }
-}
-
-function setTransactionDefaults (defaults) {
-  for (let name in contracts) {
-    contracts[name].defaults(defaults)
-  }
-}
-
-module.exports = Object.assign({}, contracts, {
-  setTransactionDefaults: setTransactionDefaults,
-  setProvider: setProvider
-})
+import Users from './build/contracts/Users.json'
+export { Users }

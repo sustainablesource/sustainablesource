eea8e6fb751e28e28e4d82404b8ba8e2a1d7d861
Mark Spanbroek <mark@spanbroek.net>
2018-10-15 16:05:43 +0200

    attestations: show ethereum address when provided

--- attestations/src/Attest.js
+++ attestations/src/Attest.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export const Attest = ({ reason, githubUsername }) => (<div>
+export const Attest = ({ reason, githubUsername, ethereumAddress }) => (<div>
   <Reason>{ reason }</Reason>
   <GithubUsername username={githubUsername} />
+  <EthereumAddress address={ethereumAddress} />
 </div>)
 
 const Reason = ({ children }) => (<p>{ children }</p>)
 const GithubUsername = ({ username }) => (<p>{ username }</p>)
+const EthereumAddress = ({ address }) => (<p>{ address }</p>)
--- attestations/src/Attest.test.js
+++ attestations/src/Attest.test.js
@@ -4,6 +4,7 @@ import { Attest } from './Attest'
 
 const reason = 'in order to collect your income'
 const githubUsername = 'some_user'
+const ethereumAddress = '0x1234567890'
 
 it('explains why attestation is necessary', () => {
   const { getByText } = render(<Attest reason={reason} />)
@@ -13,4 +14,9 @@ it('explains why attestation is necessary', () => {
 it('shows github username when provided', () => {
   const { getByText } = render(<Attest githubUsername={githubUsername} />)
   expect(getByText(githubUsername)).toBeInTheDocument()
+})
+
+it('shows ethereum address when provided', () => {
+  const { getByText } = render(<Attest ethereumAddress={ethereumAddress} />)
+  expect(getByText(ethereumAddress)).toBeInTheDocument()
 })

8377fca2c8075e760327f19c68578204584e6d4d
Mark Spanbroek <mark@spanbroek.net>
2018-10-16 20:26:00 +0200

    attestations: format using prettier

--- attestations/src/Attest.js
+++ attestations/src/Attest.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Button from 'react-bootstrap/lib/Button'
 
-export const Attest = ({ reason, username, address }) => (<div>
-  <Reason>{ reason }</Reason>
-  <Github username={username} />
-  <EthereumAddress address={address} />
-  <Button disabled={!username || !address}>register</Button>
-</div>)
+export const Attest = ({ reason, username, address }) => (
+  <div>
+    <Reason>{reason}</Reason>
+    <Github username={username} />
+    <EthereumAddress address={address} />
+    <Button disabled={!username || !address}>register</Button>
+  </div>
+)
 
-const Reason = ({ children }) => (<p>{ children }</p>)
+const Reason = ({ children }) => <p>{children}</p>
 const Github = ({ username }) => {
   if (username) {
     return <GithubUsername username={username} />
@@ -16,6 +18,6 @@ const Github = ({ username }) => {
     return <GithubLogin />
   }
 }
-const GithubUsername = ({ username }) => (<p>{ username }</p>)
+const GithubUsername = ({ username }) => <p>{username}</p>
 const GithubLogin = () => <Button>login to Github</Button>
-const EthereumAddress = ({ address }) => (<p>{ address }</p>)
+const EthereumAddress = ({ address }) => <p>{address}</p>

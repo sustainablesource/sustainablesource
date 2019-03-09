95ed14bbcd26009b34dc36936b63ee4fde043e2d
Mark Spanbroek <mark@spanbroek.net>
2019-03-09 15:15:06 +0100

    [user-registry-ui] remove duplication in OAuthCallback test

--- user-registry-ui/src/github/OAuthCallback.test.js
+++ user-registry-ui/src/github/OAuthCallback.test.js
@@ -5,32 +5,34 @@ import { OAuthCallback } from './OAuthCallback'
 const localStorage = window.localStorage
 const location = window.location
 
-const accessToken = 'some_access_token'
-const tokenType = 'some_token_type'
+describe('when access token in url', () => {
+  const accessToken = 'some_access_token'
+  const tokenType = 'some_token_type'
 
-it('stores the access token in local storage', () => {
-  location.href += `#access_token=${accessToken}&token_type=${tokenType}`
-  render(<OAuthCallback />)
-  const serialized = JSON.stringify({ accessToken, tokenType })
-  expect(localStorage.setItem).toBeCalledWith('oauth', serialized)
-})
+  beforeEach(() => {
+    location.href += `#access_token=${accessToken}&token_type=${tokenType}`
+  })
 
-it('removes the access token from the url', () => {
-  const originalUrl = location.href
-  location.href += `#access_token=${accessToken}&token_type=${tokenType}`
-  render(<OAuthCallback />)
-  expect(location.assign).toBeCalledWith(originalUrl)
+  it('stores the access token in local storage', () => {
+    render(<OAuthCallback />)
+    const serialized = JSON.stringify({ accessToken, tokenType })
+    expect(localStorage.setItem).toBeCalledWith('oauth', serialized)
+  })
+
+  it('removes the access token from the url', () => {
+    render(<OAuthCallback />)
+    expect(location.assign).toBeCalledWith(location.origin + location.pathname)
+  })
+
+  it('does not overwrite an existing token', () => {
+    localStorage.__STORE__['oauth'] = 'an_existing_token'
+    render(<OAuthCallback />)
+    expect(localStorage.setItem).not.toBeCalled()
+  })
 })
 
 it('does nothing when access token is not present', () => {
   render(<OAuthCallback />)
   expect(localStorage.setItem).not.toBeCalled()
   expect(location.assign).not.toBeCalled()
-})
-
-it('does not overwrite an existing token', () => {
-  localStorage.__STORE__['oauth'] = 'an_existing_token'
-  location.href += `#access_token=${accessToken}&token_type=${tokenType}`
-  render(<OAuthCallback />)
-  expect(localStorage.setItem).not.toBeCalled()
 })

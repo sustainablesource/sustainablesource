bdae899667f2eb13c6ec25ab2549514567c09f46
Mark Spanbroek <mark@spanbroek.net>
2019-06-09 21:20:44 +0200

    Move redux store creation into a separate file

--- user-registry-ui/pages/index.js
+++ user-registry-ui/pages/index.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import 'semantic-ui-offline/semantic.min.css'
-import { configureStore } from 'redux-starter-kit'
 import { Provider } from 'react-redux'
 import { Wizard } from '../wizard'
-import { githubReducer } from '../github'
-import { ethereumReducer } from '../ethereum'
+import { createStore } from '../store'
 
-const store = configureStore({ reducer: {
-  github: githubReducer,
-  ethereum: ethereumReducer
-} })
+const store = createStore()
 
 const Registration = () => (
   <Provider store={store}>

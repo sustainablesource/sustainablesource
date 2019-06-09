054ce96d218793725cb4b2540f464f943fe55330
Mark Spanbroek <mark@spanbroek.net>
2019-06-09 17:31:54 +0200

    Fix CSS imports in Jest

--- user-registry-ui/jest.config.js
+++ user-registry-ui/jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   coverageReporters: [
     'text-summary'
   ],
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy'
+  },
   setupFilesAfterEnv: [
     '@testing-library/react/cleanup-after-each',
     'jest-dom/extend-expect'

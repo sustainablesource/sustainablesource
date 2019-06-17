45ae9a1c740107c250d450133c14d1fd33e93a97
Mark Spanbroek <mark@spanbroek.net>
2019-06-17 13:08:09 +0200

    Fix Truffle error: "Cannot convert undefined or null to object"

--- integration-tests/contracts/Imports.sol
+++ integration-tests/contracts/Imports.sol
@@ -3,3 +3,5 @@ pragma solidity ^0.5.0;
 import "@sustainablesource/user-registry/contracts/Users.sol";
 import "@sustainablesource/contracts/contracts/SustainableSource.sol";
 import "@sustainablesource/contracts/contracts/PullRequests.sol";
+
+contract Imports {}

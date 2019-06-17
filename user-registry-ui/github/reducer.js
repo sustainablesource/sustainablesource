6eafc335260af7c451a517a159971aad3f2fda52
Mark Spanbroek <mark@spanbroek.net>
2019-06-17 16:14:52 +0200

    Add redux actions for storing OAuth tokens

--- user-registry-ui/github/reducer.js
+++ user-registry-ui/github/reducer.js
@@ -1,3 +1,13 @@
 import { createReducer } from 'redux-starter-kit'
+import { storeOAuthToken, clearOAuthToken } from './actions'
 
-export const githubReducer = createReducer({}, {})
+export const githubReducer = createReducer({}, {
+  [storeOAuthToken]: (state, action) => {
+    if (!state.oauthToken) {
+      state.oauthToken = action.payload
+    }
+  },
+  [clearOAuthToken]: (state, action) => {
+    delete state.oauthToken
+  }
+})

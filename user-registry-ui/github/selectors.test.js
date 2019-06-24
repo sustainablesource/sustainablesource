0139aca0dd45dd910b9edf169ad872e566c76a66
Mark Spanbroek <mark@spanbroek.net>
2019-06-24 21:49:18 +0200

    Add selector for OAuth token

--- user-registry-ui/github/selectors.test.js
+++ user-registry-ui/github/selectors.test.js
@@ -1,6 +1,12 @@
-import { getGithubUsername } from './selectors'
+import { getGithubUsername, getGithubOAuthToken } from './selectors'
 
 it('selects the github username', () => {
   const username = 'some user name'
   expect(getGithubUsername({ github: { username } })).toEqual(username)
+})
+
+it('selects the oauth token', () => {
+  const accessToken = 'some_token'
+  const state = { github: { oauth: { accessToken } } }
+  expect(getGithubOAuthToken(state)).toEqual(accessToken)
 })

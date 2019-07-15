05bb123b65141d9b6d6461363b423cab9e43f139
Mark Spanbroek <mark@spanbroek.net>
2019-07-15 10:21:22 +0200

    Add OAuthCallback to main page instead of separate page.

--- user-registry-ui/pages/oauth-callback.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import OAuthCallback from './oauth-callback'
-import { render } from '@testing-library/react'
-
-it('renders', () => {
-  expect(() => render(<OAuthCallback />)).not.toThrow()
-})

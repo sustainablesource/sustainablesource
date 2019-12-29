c2c672ce0b316304e6f53ecd870ec765bbf301be
Mark Spanbroek <mark@spanbroek.net>
2019-12-29 20:33:34 +0100

    Show wallet connect qr code

--- user-registry-ui/wizard/prompt/EthereumPrompt.js
+++ user-registry-ui/wizard/prompt/EthereumPrompt.js
@@ -1,9 +1,14 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { connectToWallet } from '../../ethereum'
+import QRCode from 'qrcode.react'
+import { useDispatch, useSelector } from 'react-redux'
+import { connectToWallet, getWalletUri } from '../../ethereum'
 
 export const EthereumPrompt = () => {
   const dispatch = useDispatch()
   useEffect(() => dispatch(connectToWallet()), [])
-  return <div data-testid='ethereum-prompt' />
+  return (
+    <div data-testid='ethereum-prompt'>
+      <QRCode data-testid='qrcode' value={useSelector(getWalletUri)} />
+    </div>
+  )
 }
--- user-registry-ui/wizard/prompt/EthereumPrompt.test.js
+++ user-registry-ui/wizard/prompt/EthereumPrompt.test.js
@@ -6,8 +6,16 @@ import { EthereumPrompt } from './EthereumPrompt'
 
 const walletUri = 'some:walletconnect:uri'
 
-it('initiates wallet connection when shown', () => {
+beforeEach(() => {
   mockUri(walletUri)
+})
+
+it('initiates wallet connection when shown', () => {
   const { store } = renderWithRedux(<EthereumPrompt />)
   expect(getWalletUri(store.getState())).toEqual(walletUri)
+})
+
+it('shows a wallet connect qr code', async () => {
+  const { getByTestId } = renderWithRedux(<EthereumPrompt />)
+  expect(getByTestId('qrcode').getAttribute('value')).toEqual(walletUri)
 })

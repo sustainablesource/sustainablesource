ad70cc63c28ab85b4a38553a86a51ca03759f2c8
Mark Spanbroek <mark@spanbroek.net>
2020-01-03 11:26:56 +0100

    Show github username and ethereum account on confirmation screen

--- user-registry-ui/wizard/prompt/ConfirmationPrompt.js
+++ user-registry-ui/wizard/prompt/ConfirmationPrompt.js
@@ -1,5 +1,25 @@
 import React from 'react'
+import { Button, Container, Divider, List, Segment } from 'semantic-ui-react'
+import { useSelector } from 'react-redux'
+import { getGithubUsername } from '../../github'
+import { getEthereumAccount } from '../../ethereum'
 
-export const ConfirmationPrompt = () => (
-  <div data-testid='confirmation-prompt' />
-)
+export const ConfirmationPrompt = () => {
+  const username = useSelector(getGithubUsername)
+  const account = useSelector(getEthereumAccount)
+  return (
+    <Container text data-testid='confirmation-prompt'>
+      You're about to publicly register your Github username and Ethereum
+      account for receiving Sustainable Source payouts. Is this what you want?
+      <Divider hidden />
+      <List>
+        <List.Item icon='github' description={username} />
+        <List.Item icon='ethereum' description={account} />
+      </List>
+      <Divider hidden />
+      <Segment basic textAlign='center'>
+        <Button primary content='Register' />
+      </Segment>
+    </Container>
+  )
+}
---
+++ user-registry-ui/wizard/prompt/ConfirmationPrompt.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { renderWithRedux } from '../../store/testrender'
+import { ConfirmationPrompt } from './ConfirmationPrompt'
+
+const username = 'some user name'
+const account = '0xSomeAccount'
+
+it('shows github account used for registration', () => {
+  const state = { github: { username } }
+  const { queryByText } = renderWithRedux(<ConfirmationPrompt />, state)
+  expect(queryByText(username, { exact: false })).not.toBeNull()
+})
+
+it('shows ethereum account used for registration', () => {
+  const state = { ethereum: { account } }
+  const { queryByText } = renderWithRedux(<ConfirmationPrompt />, state)
+  expect(queryByText(account, { exact: false })).not.toBeNull()
+})

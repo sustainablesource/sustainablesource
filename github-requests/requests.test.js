c2cc8f62e3a47de372b53137451eb2d30d7e559b
Mark Spanbroek <mark@spanbroek.net>
2020-01-13 13:55:03 +0100

    Remove duplication

--- github-requests/requests.test.js
+++ github-requests/requests.test.js
@@ -1,105 +1,98 @@
 import fetchMock from 'node-fetch'
 import { get, post, HttpError } from './requests'
 
-describe('http get request', () => {
+describe('http requests', () => {
   const accessToken = 'some_token'
   const path = '/some/path'
   const url = `https://api.github.com${path}`
+  const result = { some: 'result' }
 
-  describe('success', () => {
-    const result = { some: 'result' }
+  describe('get', () => {
+    describe('success', () => {
+      let response
 
-    beforeEach(() => {
-      fetchMock.get(url, result)
+      beforeEach(async () => {
+        fetchMock.get(url, result)
+        response = await get(path, accessToken)
+      })
+
+      it('provides oauth authorization', checkAuthorization)
+      it('requests a json response', checkAccept)
+
+      it('returns the response', async () => {
+        expect(response).toEqual(result)
+      })
     })
 
-    it('provides oauth authorization', async () => {
-      await get(path, accessToken)
-      const authorization = fetchMock.lastOptions().headers['Authorization']
-      expect(authorization).toEqual(`token ${accessToken}`)
-    })
+    describe('failure', () => {
+      const status = 404
+      const message = 'not found'
 
-    it('requests a json response', async () => {
-      await get(path, accessToken)
-      const accept = fetchMock.lastOptions().headers['Accept']
-      expect(accept).toEqual('application/json')
-    })
+      beforeEach(() => {
+        fetchMock.get(url, { status, body: { message } })
+      })
 
-    it('returns the response', async () => {
-      expect(await get(path, accessToken)).toEqual(result)
-    })
-  })
-
-  describe('failure', () => {
-    const status = 404
-    const message = 'not found'
-
-    beforeEach(() => {
-      fetchMock.get(url, { status, body: { message } })
-    })
-
-    it('throws on http errors', async () => {
-      const retrieval = get(path, accessToken)
-      const expected = new HttpError({ status, message })
-      await expect(retrieval).rejects.toStrictEqual(expected)
-    })
-  })
-})
-
-describe('http post request', () => {
-  const accessToken = 'some_token'
-  const path = '/some/path'
-  const url = `https://api.github.com${path}`
-  const body = { some: 'body' }
-
-  describe('success', () => {
-    const result = { some: 'result' }
-
-    beforeEach(() => {
-      fetchMock.post(url, result)
-    })
-
-    it('provides oauth authorization', async () => {
-      await post(path, accessToken, body)
-      const authorization = fetchMock.lastOptions().headers['Authorization']
-      expect(authorization).toEqual(`token ${accessToken}`)
-    })
-
-    it('requests a json response', async () => {
-      await post(path, accessToken, body)
-      const accept = fetchMock.lastOptions().headers['Accept']
-      expect(accept).toEqual('application/json')
-    })
-
-    it('sends json', async () => {
-      await post(path, accessToken, body)
-      const contentType = fetchMock.lastOptions().headers['Content-Type']
-      expect(contentType).toEqual('application/json')
-    })
-
-    it('sends the body', async () => {
-      await post(path, accessToken, body)
-      const sent = JSON.parse(fetchMock.lastOptions().body)
-      expect(sent).toEqual(body)
-    })
-
-    it('returns the response', async () => {
-      expect(await post(path, accessToken, body)).toEqual(result)
+      it('throws on http errors', async () => {
+        const retrieval = get(path, accessToken)
+        const expected = new HttpError({ status, message })
+        await expect(retrieval).rejects.toStrictEqual(expected)
+      })
     })
   })
 
-  describe('failure', () => {
-    const status = 404
-    const message = 'not found'
+  describe('post', () => {
+    const body = { some: 'body' }
 
-    beforeEach(() => {
-      fetchMock.post(url, { status, body: { message } })
+    describe('success', () => {
+      let response
+
+      beforeEach(async () => {
+        fetchMock.post(url, result)
+        response = await post(path, accessToken, body)
+      })
+
+      it('provides oauth authorization', checkAuthorization)
+      it('requests a json response', checkAccept)
+      it('sends json', checkContentType)
+
+      it('sends the body', async () => {
+        const sent = JSON.parse(fetchMock.lastOptions().body)
+        expect(sent).toEqual(body)
+      })
+
+      it('returns the response', async () => {
+        expect(response).toEqual(result)
+      })
     })
 
-    it('throws on http errors', async () => {
-      const retrieval = post(path, accessToken, body)
-      const expected = new HttpError({ status, message })
-      await expect(retrieval).rejects.toStrictEqual(expected)
+    describe('failure', () => {
+      const status = 404
+      const message = 'not found'
+
+      beforeEach(() => {
+        fetchMock.post(url, { status, body: { message } })
+      })
+
+      it('throws on http errors', async () => {
+        const retrieval = post(path, accessToken, body)
+        const expected = new HttpError({ status, message })
+        await expect(retrieval).rejects.toStrictEqual(expected)
+      })
     })
   })
+
+  function checkAuthorization () {
+    const authorization = fetchMock.lastOptions().headers['Authorization']
+    expect(authorization).toEqual(`token ${accessToken}`)
+  }
+
+  function checkAccept () {
+    const accept = fetchMock.lastOptions().headers['Accept']
+    expect(accept).toEqual('application/json')
+  }
+
+  function checkContentType () {
+    const contentType = fetchMock.lastOptions().headers['Content-Type']
+    expect(contentType).toEqual('application/json')
+  }
 })

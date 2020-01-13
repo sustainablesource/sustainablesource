20cc87e9c9d5d6c18d043dcce6d181ec4583d74a
Mark Spanbroek <mark@spanbroek.net>
2020-01-13 12:01:17 +0100

    Move HTTP post handling tests to requests.test.js

--- github-api/gists.test.js
+++ github-api/gists.test.js
@@ -1,57 +1,16 @@
 import fetchMock from 'node-fetch'
-import { HttpError } from './requests'
 import { createGist } from './gists'
 
-describe('creating a gist', () => {
-  const url = 'https://api.github.com/gists'
-  const accessToken = 'some_token'
-  const options = {
-    description: 'some gist',
-    files: { 'some_file_name': 'some content' }
-  }
+const url = 'https://api.github.com/gists'
+const accessToken = 'some_token'
+const options = {
+  description: 'some gist',
+  files: { 'some_file_name': 'some content' }
+}
 
-  describe('success', () => {
-    beforeEach(() => {
-      fetchMock.post(url, {})
-    })
-
-    it('provides oauth authorization', async () => {
-      await createGist({ accessToken, options })
-      const authorization = fetchMock.lastOptions().headers['Authorization']
-      expect(authorization).toEqual(`token ${accessToken}`)
-    })
-
-    it('requests a json response', async () => {
-      await createGist({ accessToken, options })
-      const accept = fetchMock.lastOptions().headers['Accept']
-      expect(accept).toEqual('application/json')
-    })
-
-    it('sends json', async () => {
-      await createGist({ accessToken, options })
-      const contentType = fetchMock.lastOptions().headers['Content-Type']
-      expect(contentType).toEqual('application/json')
-    })
-
-    it('sends the gist options', async () => {
-      await createGist({ accessToken, options })
-      const body = JSON.parse(fetchMock.lastOptions().body)
-      expect(body).toEqual(options)
-    })
-  })
-
-  describe('failure', () => {
-    const status = 404
-    const message = 'not found'
-
-    beforeEach(() => {
-      fetchMock.post(url, { status, body: { message } })
-    })
-
-    it('throws on http errors', async () => {
-      const creation = createGist({ accessToken, options })
-      const expected = new HttpError({ status, message })
-      await expect(creation).rejects.toStrictEqual(expected)
-    })
-  })
+it('creates a gist', async () => {
+  fetchMock.post(url, {})
+  await createGist({ accessToken, options })
+  const body = JSON.parse(fetchMock.lastOptions().body)
+  expect(body).toEqual(options)
 })
--- github-api/requests.test.js
+++ github-api/requests.test.js
@@ -1,5 +1,5 @@
 import fetchMock from 'node-fetch'
-import { get, HttpError } from './requests'
+import { get, post, HttpError } from './requests'
 
 describe('http get request', () => {
   const accessToken = 'some_token'
@@ -40,6 +40,64 @@ describe('http get request', () => {
 
     it('throws on http errors', async () => {
       const retrieval = get(path, accessToken)
+      const expected = new HttpError({ status, message })
+      await expect(retrieval).rejects.toStrictEqual(expected)
+    })
+  })
+})
+
+describe('http post request', () => {
+  const accessToken = 'some_token'
+  const path = '/some/path'
+  const url = `https://api.github.com${path}`
+  const body = { some: 'body' }
+
+  describe('success', () => {
+    const result = { some: 'result' }
+
+    beforeEach(() => {
+      fetchMock.post(url, result)
+    })
+
+    it('provides oauth authorization', async () => {
+      await post(path, accessToken, body)
+      const authorization = fetchMock.lastOptions().headers['Authorization']
+      expect(authorization).toEqual(`token ${accessToken}`)
+    })
+
+    it('requests a json response', async () => {
+      await post(path, accessToken, body)
+      const accept = fetchMock.lastOptions().headers['Accept']
+      expect(accept).toEqual('application/json')
+    })
+
+    it('sends json', async () => {
+      await post(path, accessToken, body)
+      const contentType = fetchMock.lastOptions().headers['Content-Type']
+      expect(contentType).toEqual('application/json')
+    })
+
+    it('sends the body', async () => {
+      await post(path, accessToken, body)
+      const sent = JSON.parse(fetchMock.lastOptions().body)
+      expect(sent).toEqual(body)
+    })
+
+    it('returns the response', async () => {
+      expect(await post(path, accessToken, body)).toEqual(result)
+    })
+  })
+
+  describe('failure', () => {
+    const status = 404
+    const message = 'not found'
+
+    beforeEach(() => {
+      fetchMock.post(url, { status, body: { message } })
+    })
+
+    it('throws on http errors', async () => {
+      const retrieval = post(path, accessToken, body)
       const expected = new HttpError({ status, message })
       await expect(retrieval).rejects.toStrictEqual(expected)
     })

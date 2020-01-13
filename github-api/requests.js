76025e978a8a9f632866bea720963b61ec01b07d
Mark Spanbroek <mark@spanbroek.net>
2020-01-13 11:36:32 +0100

    Remove duplication

--- github-api/requests.js
+++ github-api/requests.js
@@ -5,34 +5,34 @@ const url = 'https://api.github.com'
 
 export async function get (path, accessToken) {
   const response = await fetch(`${url}${path}`, {
-    headers: {
-      Authorization: `token ${accessToken}`,
-      Accept: 'application/json'
-    }
+    headers: headers(accessToken)
   })
-  if (!response.ok) {
-    const json = await response.json()
-    const status = response.status
-    const message = json.message
-    throw new HttpError({ status, message })
-  }
+  await handleError(response)
   return response.json()
 }
 
 export async function post (path, accessToken, body) {
   const response = await fetch(`${url}${path}`, {
     method: 'POST',
-    headers: {
-      Authorization: `token ${accessToken}`,
-      Accept: 'application/json'
-    },
+    headers: headers(accessToken),
     body: JSON.stringify(body)
   })
+  await handleError(response)
+  return response.json()
+}
+
+function headers (accessToken) {
+  return {
+    Authorization: `token ${accessToken}`,
+    Accept: 'application/json'
+  }
+}
+
+async function handleError (response) {
   if (!response.ok) {
     const json = await response.json()
     const status = response.status
     const message = json.message
     throw new HttpError({ status, message })
   }
-  return response.json()
 }

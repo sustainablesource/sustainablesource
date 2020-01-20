0bffa9886641f9f3548d8b45416a7c0e70eb7b79
Mark Spanbroek <mark@spanbroek.net>
2020-01-20 13:44:16 +0100

    Add registration reducer to store

--- user-registry-ui/store/store.js
+++ user-registry-ui/store/store.js
@@ -2,12 +2,14 @@ import { configureStore, getDefaultMiddleware } from 'redux-starter-kit'
 import { load, save } from 'redux-localstorage-simple'
 import { githubReducer } from '../github'
 import { ethereumReducer } from '../ethereum'
+import { registrationReducer } from '../registration'
 import { usernameRetriever } from '../github/middleware'
 
 export const createStore = state => configureStore({
   reducer: {
     github: githubReducer,
-    ethereum: ethereumReducer
+    ethereum: ethereumReducer,
+    registration: registrationReducer
   },
   preloadedState: state || loadIfPossible({ namespace: 'store' }),
   middleware: [

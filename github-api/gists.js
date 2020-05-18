b02bbee46b21091ad97b8828d5bded0af9dc38f6
Mark Spanbroek <mark@spanbroek.net>
2020-05-18 10:34:10 +0200

    github-api: retreive a gist

--- github-api/gists.js
+++ github-api/gists.js
@@ -1,7 +1,11 @@
-import { post, del } from '@sustainablesource/github-requests'
+import { post, get, del } from '@sustainablesource/github-requests'
 
 export async function createGist ({ accessToken, options }) {
   return (await post('/gists', accessToken, options)).id
+}
+
+export async function getGist ({ accessToken, id }) {
+  return get(`/gists/${id}`, accessToken)
 }
 
 export async function deleteGist ({ accessToken, id }) {
--- github-api/gists.test.js
+++ github-api/gists.test.js
@@ -1,17 +1,23 @@
-import { post, del } from '@sustainablesource/github-requests'
-import { createGist, deleteGist } from './gists'
+import { post, get, del } from '@sustainablesource/github-requests'
+import { createGist, getGist, deleteGist } from './gists'
 
 const accessToken = 'some_token'
-const options = {
+const id = 'some_id'
+const gist = {
   description: 'some gist',
   files: { some_file_name: 'some content' }
 }
-const id = 'some id'
 
 it('creates a gist', async () => {
   post.mockResolvedValue({ id })
-  expect(await createGist({ accessToken, options })).toEqual(id)
-  expect(post).toBeCalledWith('/gists', accessToken, options)
+  expect(await createGist({ accessToken, options: gist })).toEqual(id)
+  expect(post).toBeCalledWith('/gists', accessToken, gist)
+})
+
+it('retrieves a gist', async () => {
+  get.mockResolvedValue(gist)
+  expect(await getGist({ accessToken, id })).toEqual(gist)
+  expect(get).toBeCalledWith(`/gists/${id}`, accessToken)
 })
 
 it('deletes a gist', async () => {
